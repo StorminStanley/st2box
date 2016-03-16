de9936b8809e50ab26467cd6fa22d57552bbdd93
Kirill Enykeev <enykeev@gmail.com>
2016-03-16 12:44:43 +0600

    Add mistral containers

---
+++ mistral/docker-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Generate config file
+generate_config_file() {
+  # Configuration has been already altered, so skip generation!
+  (md5sum --quiet -c /mistral.conf.orig.md5) || return 0
+
+  POSTGRES_USER=${POSTGRES_USER:-mistral}
+  POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-StackStorm}
+  POSTGRES_HOST=${POSTGRES_HOST:-postgres}
+  POSTGRES_DB=${POSTGRES_DB:-mistral}
+
+  RABBITMQ_USER=${RABBITMQ_USER:-guest}
+  RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD:-guest}
+  RABBITMQ_HOST=${RABBITMQ_HOST:-rabbitmq}
+
+  cat /mistral.conf.template \
+    | sed -r "s|^(connection.=.).*|\1postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB|" \
+    | sed -r "s|^(transport_url.=.).*|\1rabbit://$RABBITMQ_USER:$RABBITMQ_PASSWORD@$RABBITMQ_HOST|" \
+    > /etc/mistral/mistral.conf
+}
+
+generate_config_file
+
+populate_db() {
+  /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf upgrade head
+  /opt/stackstorm/mistral/bin/mistral-db-manage --config-file /etc/mistral/mistral.conf populate
+}
+
+case "$MISTRAL_SERVICE" in
+"api")
+  populate_db
+  ARGS="${@:---log-file=- -b 0.0.0.0:8989 -w 2 --graceful-timeout 10 mistral.api.wsgi}"
+  CMD="/opt/stackstorm/mistral/bin/gunicorn $ARGS"
+  ;;
+"engine")
+  populate_db
+  ARGS="${@:---config-file /etc/mistral/mistral.conf}"
+  CMD="/opt/stackstorm/mistral/bin/mistral-server --server engine $ARGS"
+  ;;
+"executor")
+  ARGS="${@:---config-file /etc/mistral/mistral.conf}"
+  CMD="/opt/stackstorm/mistral/bin/mistral-server --server executor $ARGS"
+  ;;
+*)
+  CMD="$@"
+esac
+
+exec $CMD
